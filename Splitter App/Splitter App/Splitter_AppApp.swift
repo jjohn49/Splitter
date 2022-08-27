1b0e8b93888f338a280c8362826c70629ad80803
jjohns49 <jjohns49@villanova.edu>
2022-08-27 16:49:31 -0400

    Did this work?

---
+++ Splitter App/Splitter App/Splitter_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Splitter_AppApp.swift
+//  Splitter App
+//
+//  Created by John Johnston on 8/27/22.
+//
+
+import SwiftUI
+
+@main
+struct Splitter_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
